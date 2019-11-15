c17def3df7525c16b649d53f9f8da7bd9f79df7a
zhangzx <zhangzx@allinfinance.com>
2019-11-15 17:51:24 +0800

    1-3 并发编程初体验（例子）

---
+++ src/main/java/com/mmall/demo/CountExample.java
@@ -0,0 +1,45 @@
+package com.mmall.demo;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+
+/**
+ * @ClassName CountExample
+ * @Description
+ * @Author zhangzx
+ * @Date 2019/11/13 16:12
+ * Version 1.0
+ **/
+@Slf4j
+public class CountExample {
+
+    private static int threadTotal = 200;
+    private static int clientTotal = 5000;
+    private static long count = 0;
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        for (int index = 0; index < clientTotal; index ++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                } catch (Exception e) {
+                    log.error("exception", e);
+                }
+            });
+        }
+        executorService.shutdown();
+        log.info("count:{}", count);
+    }
+
+    private static void add() {
+        count ++;
+    }
+}

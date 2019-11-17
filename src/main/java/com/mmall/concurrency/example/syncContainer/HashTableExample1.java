1c09058e1bbd75a17baa1c34cf99e8f847c2cc47
zhangzx <zhangzx@allinfinance.com>
2019-11-17 16:10:21 +0800

    5-6 同步容器1 Vector的线程安全和不安全情况 Stack线程安全 HashTable线程安全

---
+++ src/main/java/com/mmall/concurrency/example/syncContainer/HashTableExample1.java
@@ -0,0 +1,53 @@
+package com.mmall.concurrency.example.syncContainer;
+
+import com.mmall.concurrency.annotations.NotThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * @ClassName ArrayListExample
+ * @Description
+ * @Author zhangzx
+ * @Date 2019/11/17 13:40
+ * Version 1.0
+ **/
+@Slf4j
+public class HashTableExample1 {
+    public static int clientTotal = 5000;
+    public static int threadTotal = 200;
+
+    private static Map<Integer, Integer> map = new Hashtable<>();
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i ++) {
+            final int count = i;
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    update(count);
+                    semaphore.release();
+                } catch (Exception e) {
+                    log.error("exception", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();// 此方法保证count必减为0
+        executorService.shutdown();
+        log.info("size:{}", map.size());
+    }
+
+    private static void update(int i) {
+        map.put(i, i);
+    }
+}

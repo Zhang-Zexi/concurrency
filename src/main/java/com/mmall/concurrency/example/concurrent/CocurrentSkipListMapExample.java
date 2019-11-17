bf2b79012e7cafc224438a9489b0716273144a69
zhangzx <zhangzx@allinfinance.com>
2019-11-17 17:57:23 +0800

    5-8 并发容器的介绍 CopyOnWriteArrayList CopyOnWriteArraySet/ConcurrentSkipListSet ConcurrentHashMap/ConcurrentSkipListMap

---
+++ src/main/java/com/mmall/concurrency/example/concurrent/CocurrentSkipListMapExample.java
@@ -0,0 +1,49 @@
+package com.mmall.concurrency.example.concurrent;
+
+import com.mmall.concurrency.annotations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+import java.util.concurrent.*;
+
+/**
+ * @ClassName ArrayListExample
+ * @Description
+ * @Author zhangzx
+ * @Date 2019/11/17 13:40
+ * Version 1.0
+ **/
+@Slf4j
+@ThreadSafe
+public class CocurrentSkipListMapExample {
+    public static int clientTotal = 5000;
+    public static int threadTotal = 200;
+
+    private static Map<Integer, Integer> map = new ConcurrentSkipListMap<>();
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i ++) {
+            final int count = i;
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    update(count);
+                    semaphore.release();
+                } catch (Exception e) {
+                    log.error("exception", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();// 此方法保证count必减为0
+        executorService.shutdown();
+        log.info("size:{}", map.size());
+    }
+
+    private static void update(int i) {
+        map.put(i, i);
+    }
+}
---
+++ src/main/java/com/mmall/concurrency/example/concurrent/CopyOnWriteArraySetExample.java
@@ -0,0 +1,49 @@
+package com.mmall.concurrency.example.concurrent;
+
+import com.mmall.concurrency.annotations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Set;
+import java.util.concurrent.*;
+
+/**
+ * @ClassName ArrayListExample
+ * @Description
+ * @Author zhangzx
+ * @Date 2019/11/17 13:40
+ * Version 1.0
+ **/
+@Slf4j
+@ThreadSafe
+public class CopyOnWriteArraySetExample {
+    public static int clientTotal = 5000;
+    public static int threadTotal = 200;
+
+    private static Set<Integer> set = new CopyOnWriteArraySet<>();
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i ++) {
+            final int count = i;
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    update(count);
+                    semaphore.release();
+                } catch (Exception e) {
+                    log.error("exception", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();// 此方法保证count必减为0
+        executorService.shutdown();
+        log.info("size:{}", set.size());
+    }
+
+    private static void update(int i) {
+        set.add(i);
+    }
+}

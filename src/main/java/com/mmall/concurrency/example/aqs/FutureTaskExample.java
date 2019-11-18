5108db641c4d6095de8229a63c53b5feacb89c41
zhangzx <zhangzx@allinfinance.com>
2019-11-18 17:41:09 +0800

    7-1 FutureTask Future/Callable/FutureTask的使用

---
+++ src/main/java/com/mmall/concurrency/example/aqs/FutureTaskExample.java
@@ -0,0 +1,34 @@
+package com.mmall.concurrency.example.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
+/**
+ * @ClassName FutureTaskExample
+ * @Description
+ * @Author zhangzx
+ * @Date 2019/11/18 17:33
+ * Version 1.0
+ **/
+@Slf4j
+public class FutureTaskExample {
+
+    public static void main(String[] args) throws Exception {
+        FutureTask<String> futureTask = new FutureTask<String>(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                log.info("do something in callable");
+                Thread.sleep(5000);
+                return "Done";
+            }
+        });
+
+        new Thread(futureTask).start();
+        log.info("do something in main");
+        Thread.sleep(1000);
+        String result = futureTask.get();
+        log.info("result: {}",result);
+    }
+}

ec2a4af1341542750d72cf9bc0d3659956d46688
zhangzx <zhangzx@allinfinance.com>
2019-12-19 15:24:57 +0800

    使用阿里巴巴开发者规范来优化线程池的使用 避免资源的浪费

--- src/main/java/com/mmall/demo/CountExample.java
+++ src/main/java/com/mmall/demo/CountExample.java
@@ -2,9 +2,7 @@ package com.mmall.demo;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.*;
 
 
 /**
@@ -17,11 +15,14 @@ import java.util.concurrent.Semaphore;
 @Slf4j
 public class CountExample {
 
+//    HashMap hashMap = new HashMap<>(10);
     private static int threadTotal = 200;
     private static int clientTotal = 5000;
     private static long count = 0;
 
     public static void main(String[] args) {
+        //最近阿里发布的 Java开发手册中强制线程池不允许使用 Executors 去创建，而是通过 ThreadPoolExecutor 的方式，这样的处理方式让写的同学更加明确线程池的运行规则，规避资源耗尽的风险
+//        ExecutorService executorService1 = new ThreadPoolExecutor(0, Integer.MAX_VALUE, 60L, TimeUnit.SECONDS,new SynchronousQueue<Runnable>());
         ExecutorService executorService = Executors.newCachedThreadPool();
         final Semaphore semaphore = new Semaphore(threadTotal);
         for (int index = 0; index < clientTotal; index ++) {
